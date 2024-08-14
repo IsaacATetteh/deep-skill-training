6387ac6c755d02f3f1638242f6401a1a4b8dd8db
Isaac <iabbeytetteh@gmail.com>
2024-08-14 09:54:54 +0100

    Fixes #7

--- components/Info.tsx
+++ components/Info.tsx
@@ -36,8 +36,8 @@ const Info = memo(() => {
               <Image
                 alt="Richard Holmes"
                 src="/coach.png"
-                width={100}
-                height={100}
+                width={250}
+                height={250}
                 className="mx-auto inset-0 h-64 md:h-72 lg:h-96 w-auto lg:mt-16 mb-20 rounded-full animated-border"
               />
             </div>
@@ -53,7 +53,51 @@ const Info = memo(() => {
                   scholarship athletes at USA division 1 schools and those just
                   starting out or playing at a recreational level.`}
                 </p>
+                <p className="pb-10">
+                  {`Current Stirling Albion Academy Coach working with the U16s,
+                  U20s and Men’s First Team. Prior experience includes coaching
+                  the Junior Academy (Ages 9-14) at Dundee United Community Trust
+                  along with starting out at highly competitive grassroots teams
+                  at the U18/U13 levels in London.`}
+                </p>
               </article>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="lg:py-16">
+              <article className="space-y-4 text-lg">
+                <h3 className="text-3xl font-semibold">Training Philosophy</h3>
+                <p>
+                  {`The best players are the best problem solvers, for this to
+                  happen our training has to give us constant problems for us to
+                  solve along with having to adapt to a limitless variety of
+                  situations rather than training the same repeatable problem over
+                  and over again. By training with us, you will learn that all the
+                  movements you make are forms of skill and that ‘deep skill’ is
+                  the ability as well as adaptability to perform in a variety of
+                  environments. All sessions are in line with this and is tailored
+                  to activate your ‘deep skill’.`}
+                </p>
+                <p>
+                  {`The second picture shows the OLIVER football tracker which is
+                  the same technology used by the Barça Academy. Deep Skill
+                  Training uses it to benchmark and measure performance from your
+                  first session with us to your last. The OLIVER football tracker
+                  records 28 metrics, giving us real data on your progress,
+                  training load and injury status.`}
+                </p>
+              </article>
+            </div>
+
+            <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+              <Image
+                alt="OLIVER football tracker"
+                src="/OLIVER.png"
+                width={250}
+                height={250}
+                className="mx-auto inset-0 h-64 md:h-72 lg:h-96 w-auto lg:mt-16 mb-20 rounded-full animated-border"
+              />
             </div>
           </div>
         </div>
@@ -61,7 +105,5 @@ const Info = memo(() => {
     </section>
   );
 });
-
-Info.displayName = "Info";
 
 export default Info;
--- components/ui/typewriter.tsx
+++ components/ui/typewriter.tsx
@@ -3,7 +3,6 @@ import { TypewriterEffect } from "../ui/typewriter-effect";
 import Checklist from "./checklist";
 import AnimatedGridPattern from "@/components/magicui/animated-grid-pattern";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export function TypewriterEffectDemo() {
   const words = [
@@ -47,14 +46,7 @@ export function TypewriterEffectDemo() {
   return (
     <div className="flex flex-col items-center justify-center h-[40rem]">
       <div className="flex flex-col items-center md:mb-36">
-        <Image
-          src="/test.png"
-          alt="DST"
-          height={384}
-          width={384}
-          className="scale-75 md:scale-100 z-10 md:mb-10"
-          priority
-        />
+        <img src="test.png" alt="DST" className="scale-75 md:scale-50 z-10" />
         <TypewriterEffect words={words} className="mb-10" />
         <Checklist />
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
