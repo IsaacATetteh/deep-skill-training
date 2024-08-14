70d4812ad6d0fcbe07145e866e1250b24522481f
Isaac <iabbeytetteh@gmail.com>
2024-08-14 19:00:02 +0100

    Final

--- components/ui/typewriter.tsx
+++ components/ui/typewriter.tsx
@@ -36,17 +36,26 @@ export function TypewriterEffectDemo() {
     className: `${word.className || ""} text-2xl md:text-5xl`.trim(),
   }));
 
+  const scrollToCalSection = () => {
+    document
+      .getElementById("cal-section")
+      ?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem]">
-      <div className="flex flex-col items-center md:mb-32">
-        <img src="test.png" alt="DST" className="scale-75 md:scale-50 z-10" />
-        <div className="mb-5 md:mb-10 h-24 flex items-center">
+    <div className="flex flex-col items-center justify-center h-[42rem]">
+      <div className="flex flex-col items-center md:mb-36">
+        <img src="test.png" alt="DST" className="scale-75 md:scale-50 z-10 " />
+        <div className="mb-5 md:mb-10 h-24 flex items-center ">
           <TypewriterEffect words={words} className="text-center" />
         </div>
         <Checklist />
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
-          <button className="z-10 underline scale-75 md:scale-100 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-            Get Started Today,
+          <button
+            onClick={scrollToCalSection}
+            className="z-10  scale-75 md:scale-100 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          >
+            Get Started Today
           </button>
         </div>
         <AnimatedGridPattern
